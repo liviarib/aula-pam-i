39bfc92554936ba1893cd1a98c4ffa7ab4b4d9db
liviarib <liviaribeiro897@gmail.com>
2025-02-26 22:49:40 -0300

    ajuste layout

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Constants from 'expo-constants'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>bem vinda!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'pink',
+    //alingItems: 'center',
     alignItems: 'center',
     justifyContent: 'center',
+    margintop: Constants.statusbarheight,
   },
 });
